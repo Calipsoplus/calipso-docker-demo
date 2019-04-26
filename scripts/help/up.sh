41df40c21c7aa5e8ceab747088c7a636f4932d4a
Àlex Camps <acampsm@cells.es>
2019-04-26 10:45:39 +0000

    fix some bugs

--- scripts/help/up.sh
+++ scripts/help/up.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
+
 cd ../../
 
-
 printf "\033[0;32mCalipsoPlus setup\033[0m\n"
-
-
 
 ip=`grep -o 'REMOTE_MACHINE_IP = "[a-zA-Z0-9.]*"'  backend/calipsoplus-backend/calipsoplus/settings_docker.py.example|awk -F"\"" '{print $2}'`
 if [ -z "$ip" ] ; then
    ip="tcp://192.168.33.13"
 fi
 
-printf "\033[1;32mEnter host/ip where docker daemon lives [\033[1;33m$ip\033[1;32m]\033[0m"
-read -p " " response
+ip_pattern=^[a-zA-Z0-9.]*$
+response="%"
+while ! [[ "${response}" =~ ${ip_pattern} ]] ;
+do
+	printf "\033[1;32mEnter host/ip where docker daemon lives [\033[1;33m$ip\033[1;32m]\033[0m"
+	read -p " " response
+done
+
  
 if [ -z "$response" ] ; then
    response=$ip
@@ -30,9 +35,17 @@ if [ -z "$logo" ] ; then
    logo="assets/images/insert-logo-here.jpg"
 fi
 
-printf "\033[1;32mEnter url logo for facility [\033[1;33m$logo\033[1;32m]\033[0m"
+logo_pattern=^[.A-Za-z0-9:/-]*$
+response="%"
+while ! [[ "${response}" =~ ${logo_pattern} ]] ;
+do
+	printf "\033[1;32mEnter url logo for facility [\033[1;33m$logo\033[1;32m]\033[0m"
+        read -p " " response
+done
 
-read -p " " response
+
+
+
 
 if [ -z "$response" ] ; then
    response=$logo
---
+++ setup.sh
@@ -0,0 +1,5 @@
+cd scripts/help
+./up.sh
+echo "Now you can build with: docker-compose build"
+echo "And then run with docker-compose up"
+echo ""
