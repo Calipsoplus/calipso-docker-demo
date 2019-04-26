13d4599d4db7a087283c342c02002e6cee6b4b6f
Àlex Camps <acampsm@cells.es>
2019-04-26 14:41:23 +0000

    r

--- scripts/help/up.sh
+++ scripts/help/up.sh
@@ -2,6 +2,8 @@
 
 cd ../../
 
+cp config/settings/settings_docker.py.example backend/calipsoplus-backend/calipsoplus/settings_docker.py.example
+
 printf "\033[0;32mCalipsoPlus setup\033[0m\n"
 
 ip=`grep -o 'REMOTE_MACHINE_IP = "[a-zA-Z0-9.]*"'  backend/calipsoplus-backend/calipsoplus/settings_docker.py.example|awk -F"\"" '{print $2}'`
@@ -23,7 +25,10 @@ if [ -z "$response" ] ; then
 fi
 
 sed -i "s#REMOTE_MACHINE_IP = \"$ip\"#REMOTE_MACHINE_IP = \"$response\"#g"  backend/calipsoplus-backend/calipsoplus/settings_docker.py.example
-sed -i "s#DOCKER_URL_DAEMON = \"tcp://[a-zA-Z0-9.]*:2357\"#DOCKER_URL_DAEMON = \"tcp://$response:2357\"#g"  backend/calipsoplus-backend/calipsoplus/settings_docker.py.example
+sed -i "s#DOCKER_URL_DAEMON = \"tcp://[a-zA-Z0-9.]*:2375\"#DOCKER_URL_DAEMON = \"tcp://$response:2375\"#g"  backend/calipsoplus-backend/calipsoplus/settings_docker.py.example
+
+cp backend/calipsoplus-backend/calipsoplus/settings_docker.py.example config/settings/settings_docker.py.example
+
 
 sed -i 's/\"browserTarget\": \"calipsoplus-app:build:docker\"/\"disableHostCheck\": true,\"browserTarget\":\"calipsoplus-app:build:docker\"/g' frontend/calipsoplus-frontend/angular.json
 
@@ -52,5 +57,10 @@ if [ -z "$response" ] ; then
 fi
 
 sed -i "s#facilityLogo: '[.A-Za-z0-9:/-]*'#facilityLogo: '$response'#g"  frontend/calipsoplus-frontend/src/environments/environment.docker.ts.example
+
+
+sed -i 's/ALLOW_LOCAL_AUTHENTICATION = 0/ALLOW_LOCAL_AUTHENTICATION = 1/g'  backend/calipsoplus-backend/calipsoplus/settings_calipso.py.example
+
+
 
 
--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
-cd scripts/help
-./up.sh
-echo "Now you can build with: docker-compose build"
-echo "And then run with docker-compose up"
-echo ""
+	
+
+
+if [ -d "backend/calipsoplus-backend" ] && [ -d "frontend/calipsoplus-frontend" ] && [ -d "mock-login/calipsoplus-local-login-mock" ]
+then
+	cd scripts/help
+	./up.sh
+	echo "Now you can build with: docker-compose build"
+	echo "And then run with docker-compose up"
+	echo ""
+else
+	echo "You have to clone all he source code before run this setup."
+fi
+
